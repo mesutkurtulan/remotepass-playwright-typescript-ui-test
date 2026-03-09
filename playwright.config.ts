c8ecbd7b20397de441e8a994e42fe1311216bc66
mesutkurtulan <mstkrtln@gmail.com>
2026-03-09 09:38:28 +0300

    update config

--- playwright.config.ts
+++ playwright.config.ts
@@ -42,15 +42,9 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
     },
   ],
 });
